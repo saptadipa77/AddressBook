77ab1fba39770d07329b19a2c5c7663963ea337f
saptadipa77 <saptadipadatta5@gmail.com>
2025-12-11 20:28:24 +0530

    start

---
+++ AddressBookNameMain.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AddressBookNameMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program\n");
+    }
+}
